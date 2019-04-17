190b6c74eea863db24624e707d7eb199d90c914d
Ganesha Shettigara <ganesha.shettigara@gmail.com>
2019-04-16 22:41:10 -0400

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    // LeetCode #64.  Minimum Path Sum
+
+    static int MinPathSum(int[][] arr)
+    {
+        int result = 0;
+
+       
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        
+
+        int[][] arr = new int[3][];
+
+        for (int arrRowItr = 0; arrRowItr < 3; arrRowItr++)
+        {
+            arr[arrRowItr] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+       int sum = MinPathSum(arr);
+
+        
+    }
+}
